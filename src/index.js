223141821cc2e8ad8d810a6708341a28fc9f49e1
Maxim Samoilov <samoilowmaxim@gmail.com>
2016-07-17 17:08:47 +0700

    feat: add tokenize and build ast for (+ 1 2)

---
+++ src/index.js
@@ -0,0 +1,56 @@
+const U = require('./utils')
+
+// tokens: '(', ')', identifier, string, number
+
+// splitIntoTokens :: String -> [String]
+const splitIntoTokens = code => {
+  return code
+    .replace(/\)/g, ' ) ')
+    .replace(/\(/g, ' ( ')
+    .split(' ')
+    .filter(U.identity)
+}
+
+const isNumber = x => /\d+/.test(x)
+// const isIdentifier = x => x.test(/[^"]+/)
+
+// buildAstFromToken :: String -> Ast
+const buildAstFromToken = token => {
+  console.log(token)
+  if (isNumber(token)) {
+    return {
+      type: 'number',
+      value: Number(token),
+    }
+  }
+
+  throw new Error('wrong token')
+}
+
+
+// buildAstFromArgs :: [String] -> [Ast]
+const buildAstFromArgs = tokens => {
+  return tokens.map(buildAstFromToken)
+}
+
+// buildAstFromTokens :: [String] -> Ast
+const buildAstFromTokens = tokens => {
+  if (tokens[0] === '(') {
+    const func = tokens[1]
+    const argsTokens = U.init(tokens.slice(2))
+    return [{
+      func,
+      args: buildAstFromArgs(argsTokens),
+    }]
+  }
+
+  throw new Error('Program should start with (')
+}
+
+module.exports = {
+  // ast :: String -> Ast
+  ast(code) {
+    const tokens = splitIntoTokens(code)
+    return buildAstFromTokens(tokens)
+  },
+}
--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,20 @@
 const { describe, it } = require('mocha')
 const { expect } = require('chai')
 
+const lisp = require('../src/')
 
 describe('test', () => {
   it('works', () => {
-    expect(1 + 1).equal(2)
+    const ast = [{
+      func: '+',
+      args: [{
+        type: 'number',
+        value: 1,
+      }, {
+        type: 'number',
+        value: 2,
+      }],
+    }]
+    expect(lisp.ast('(+ 1 2)')).eql(ast)
   })
 })
